509dc1e5c2296aa46049b29780c05799b3f0b616
monirul-fahad <immonirulfahad@gmail.com>
2023-01-23 21:02:40 +0600

    push

--- src/pages/blog-details.js
+++ src/pages/blog-details.js
@@ -22,11 +22,9 @@ const BlogDetails = () => {
   const blogDetails = data?.filter((blog) => blog?.slug === slug);
   const authorId = blogDetails[0]?.author;
 
-
-
-  const {
-    avatar_urls: { 96: url96 },
-  } = authorData;
+  // const {
+  //   avatar_urls: { 96: url96 },
+  // } = authorData;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -72,13 +70,13 @@ const BlogDetails = () => {
               <div className="author-bio" style={{ marginBottom: "30px" }}>
                 <div className="author-title-wrapper">
                   <div className="author-avatar vcard">
-                    <img
+                    {/* <img
                       alt={authorData.name}
                       src={url96}
                       className="avatar avatar-160 photo"
                       height="160"
                       width="160"
-                    />
+                    /> */}
                   </div>
                   <h2 className="author-title heading-size-4">
                     By {authorData.name}
@@ -121,6 +119,129 @@ const BlogDetails = () => {
                 aria-hidden="true"
               />
             </nav> */}
+            <hr className="styled-separator is-style-wide" aria-hidden="true" />
+            <div class="comments-wrapper section-inner">
+              <div id="respond" class="comment-respond">
+                <h2 id="reply-title" class="comment-reply-title">
+                  Leave a Reply{" "}
+                  {/* <small>
+                    <a
+                      rel="nofollow"
+                      id="cancel-comment-reply-link"
+                      href="/2022/11/18/top-elements-of-a-high-class-surround-sound-installation/#respond"
+                      style="display:none;"
+                    >
+                      Cancel reply
+                    </a>
+                  </small> */}
+                </h2>
+                <form
+                  action="https://www.projectautomate.com/wp-comments-post.php"
+                  method="post"
+                  id="commentform"
+                  class="section-inner thin max-percentage"
+                  novalidate=""
+                >
+                  <p class="comment-notes">
+                    <span id="email-notes">
+                      Your email address will not be published.
+                    </span>{" "}
+                    <span class="required-field-message">
+                      Required fields are marked <span class="required">*</span>
+                    </span>
+                  </p>
+                  <p class="comment-form-comment">
+                    <label for="comment">
+                      Comment <span class="required">*</span>
+                    </label>{" "}
+                    <textarea
+                      id="comment"
+                      name="comment"
+                      cols="45"
+                      rows="8"
+                      maxlength="65525"
+                      required=""
+                    ></textarea>
+                  </p>
+                  <p class="comment-form-author">
+                    <label for="author">
+                      Name <span class="required">*</span>
+                    </label>{" "}
+                    <input
+                      id="author"
+                      name="author"
+                      type="text"
+                      value=""
+                      size="30"
+                      maxlength="245"
+                      autocomplete="name"
+                      required=""
+                    />
+                  </p>
+                  <p class="comment-form-email">
+                    <label for="email">
+                      Email <span class="required">*</span>
+                    </label>{" "}
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      value=""
+                      size="30"
+                      maxlength="100"
+                      aria-describedby="email-notes"
+                      autocomplete="email"
+                      required=""
+                    />
+                  </p>
+                  <p class="comment-form-url">
+                    <label for="url">Website</label>{" "}
+                    <input
+                      id="url"
+                      name="url"
+                      type="url"
+                      value=""
+                      size="30"
+                      maxlength="200"
+                      autocomplete="url"
+                    />
+                  </p>
+                  <p class="comment-form-cookies-consent">
+                    <input
+                      id="wp-comment-cookies-consent"
+                      name="wp-comment-cookies-consent"
+                      type="checkbox"
+                      value="yes"
+                    />{" "}
+                    <label for="wp-comment-cookies-consent">
+                      Save my name, email, and website in this browser for the
+                      next time I comment.
+                    </label>
+                  </p>
+                  <p class="form-submit">
+                    <input
+                      name="submit"
+                      type="submit"
+                      id="submit"
+                      class="submit"
+                      value="Post Comment"
+                    />{" "}
+                    <input
+                      type="hidden"
+                      name="comment_post_ID"
+                      value="3788"
+                      id="comment_post_ID"
+                    />
+                    <input
+                      type="hidden"
+                      name="comment_parent"
+                      id="comment_parent"
+                      value="0"
+                    />
+                  </p>
+                </form>{" "}
+              </div>
+            </div>
           </article>
         )}
       </main>
