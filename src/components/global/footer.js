1bdb9a7c8ef1184543e10e55dcc0310d89f96f91
Foysal Imran <fimran2011@gmail.com>
2023-01-24 11:21:20 +0600

    update

--- src/components/global/footer.js
+++ src/components/global/footer.js
@@ -165,7 +165,7 @@ const Footer = () => {
         <div className="section-inner">
           <div className="footer-credits">
             <p className="footer-copyright">
-              © 2023 &nbsp; <Link aria-label="Home page" to="/">HOMEFIRST: automate</Link>
+              © 2023 Homefirst Automation
             </p>
 
           </div>
