acea819191da67364953bbbc7601f6fc858e06ee
Foysal Imran <fimran2011@gmail.com>
2023-01-24 13:35:23 +0600

    update

--- src/components/feature2.js
+++ src/components/feature2.js
@@ -56,15 +56,7 @@ const Feature2 = ({ title }) => {
         </p>
       </div>
       <div className="is-layout-flow wp-block-column servicesimages sidevideoleft">
-      <figure className="wp-block-image size-full eds-on-scroll eds-scroll-visible animated fadeInUp duration3">
-          <img
-            width="1915"
-            height="1440"
-            src="assets/images/home-lighting-1.webp"
-            alt=""
-            className="wp-image-839"
-          />
-        </figure>
+
         <video
           id="video-background"
           className="customsized"
