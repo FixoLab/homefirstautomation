520596ad56bc60fa88a0dc51246ec34b8dc10b2a
monirul-fahad <immonirulfahad@gmail.com>
2023-01-25 17:30:46 +0600

    connected living pushing

--- src/pages/indoor-living.js
+++ src/pages/indoor-living.js
@@ -3,7 +3,246 @@ import React from "react";
 const IndoorLiving = () => {
   return (
     <div>
-    
+        <div className="background-section" style={{backgroundImage: "url(/assets/images/indoor-1.webp)"}}>
+            <div className="background-section-content">
+                <div className="grid-items">
+                    <h2>INDOOR LIVING</h2>
+                    <h3>CAPTURE THE FEELING OF SMARTER LIVING</h3>
+                    <p>Discover how smart technology effortlessly blends into the fabric of your Southern California home.</p>
+                </div>
+                <div className="grid-items">
+                </div>
+            </div>
+        </div>
+      <div className="indoor-living-image-section">
+        <div className="title-section">
+          <div className="title-inner">
+            <div className="section-grid">
+              <div className="grid-item-1">
+                <h4>MEDIA ROOM</h4>
+                <p>
+                  Create the perfect environment for entertaining with one-touch
+                  control of an immersive audio video entertainment system
+                </p>
+              </div>
+              <div className="grid-item-2">
+                <h6>COMMON SOLUTIONS</h6>
+                <p>
+                  Smart Home Control · Media Control · Lighting Control ·
+                  High-Performance Audio · Hidden Audio Video · Universal
+                  Remotes · LED Lighting
+                </p>
+              </div>
+              <div className="grid-item-3">
+                <h6>SAMPLE SOLUTIONS</h6>
+                <div className="icon-grid">
+                  <div>
+                    <i className="fa-regular fa-lightbulb"></i>
+                  </div>
+                  <div>
+                    <i className="fa-brands fa-codepen"></i>
+                  </div>
+                  <div>
+                    <i className="fa-solid fa-house"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src="assets/images/indoor-2.webp" alt="" />
+      </div>
+      <div className="indoor-living-image-section">
+        <div className="title-section">
+          <div className="title-inner">
+            <div className="section-grid">
+              <div className="grid-item-1">
+                <h4>DEDICATED HOME THEATER</h4>
+                <p>
+                  A luxury home theater design for exceptional quality and
+                  comfort.
+                </p>
+              </div>
+              <div className="grid-item-2">
+                <h6>COMMON SOLUTIONS</h6>
+                <p>
+                  Smart Home Control · Media Control · Lighting Control ·
+                  High-Performance Audio · Hidden Audio Video · Universal
+                  Remotes · LED Lighting
+                </p>
+              </div>
+              <div className="grid-item-3">
+                <h6>SAMPLE SOLUTIONS</h6>
+                <div className="icon-grid">
+                  <div>
+                    <i className="fa-regular fa-lightbulb"></i>
+                  </div>
+                  <div>
+                    <i className="fa-brands fa-codepen"></i>
+                  </div>
+                  <div>
+                    <i className="fa-solid fa-house"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src="assets/images/indoor-3.webp" alt="" />
+      </div>
+      <div className="indoor-living-image-section">
+        <div className="title-section">
+          <div className="title-inner">
+            <div className="section-grid">
+              <div className="grid-item-1">
+                <h4>FAMILY ROOM</h4>
+                <p>
+                  Transform your moments of family fun and relaxation with
+                  solutions for enhanced comfort and revelry.
+                </p>
+              </div>
+              <div className="grid-item-2">
+                <h6>COMMON SOLUTIONS</h6>
+                <p>
+                  Smart Home Control · Media Control · Lighting Control ·
+                  High-Performance Audio · Hidden Audio Video · Universal
+                  Remotes · LED Lighting
+                </p>
+              </div>
+              <div className="grid-item-3">
+                <h6>SAMPLE SOLUTIONS</h6>
+                <div className="icon-grid">
+                  <div>
+                    <i className="fa-regular fa-lightbulb"></i>
+                  </div>
+                  <div>
+                    <i className="fa-brands fa-codepen"></i>
+                  </div>
+                  <div>
+                    <i className="fa-solid fa-house"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src="assets/images/indoor-4.webp" alt="" />
+      </div>
+      <div className="indoor-living-image-section">
+        <div className="title-section">
+          <div className="title-inner">
+            <div className="section-grid">
+              <div className="grid-item-1">
+                <h4>BEDROOM</h4>
+                <p>
+                  Experience restful slumber and energized mornings with
+                  tailored scenes that complement every Newport Beach sunrise
+                  and sunset.
+                </p>
+              </div>
+              <div className="grid-item-2">
+                <h6>COMMON SOLUTIONS</h6>
+                <p>
+                  Smart Home Control · Media Control · Lighting Control ·
+                  High-Performance Audio · Hidden Audio Video · Universal
+                  Remotes · LED Lighting
+                </p>
+              </div>
+              <div className="grid-item-3">
+                <h6>SAMPLE SOLUTIONS</h6>
+                <div className="icon-grid">
+                  <div>
+                    <i className="fa-regular fa-lightbulb"></i>
+                  </div>
+                  <div>
+                    <i className="fa-brands fa-codepen"></i>
+                  </div>
+                  <div>
+                    <i className="fa-solid fa-house"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src="assets/images/indoor-5.webp" alt="" />
+      </div>
+      <div className="indoor-living-image-section">
+        <div className="title-section">
+          <div className="title-inner">
+            <div className="section-grid">
+              <div className="grid-item-1">
+                <h4>BAR / GAME ROOM</h4>
+                <p>
+                  Host the big game with high-performance audio and sleek TV
+                  displays that reveal themselves when it’s time to entertain.
+                </p>
+              </div>
+              <div className="grid-item-2">
+                <h6>COMMON SOLUTIONS</h6>
+                <p>
+                  Smart Home Control · Media Control · Lighting Control ·
+                  High-Performance Audio · Hidden Audio Video · Universal
+                  Remotes · LED Lighting
+                </p>
+              </div>
+              <div className="grid-item-3">
+                <h6>SAMPLE SOLUTIONS</h6>
+                <div className="icon-grid">
+                  <div>
+                    <i className="fa-regular fa-lightbulb"></i>
+                  </div>
+                  <div>
+                    <i className="fa-brands fa-codepen"></i>
+                  </div>
+                  <div>
+                    <i className="fa-solid fa-house"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src="assets/images/indoor-6.webp" alt="" />
+      </div>
+      <div className="indoor-living-image-section">
+        <div className="title-section">
+          <div className="title-inner">
+            <div className="section-grid">
+              <div className="grid-item-1">
+                <h4>WHOLE HOME</h4>
+                <p>
+                  Create the perfect environment for entertaining with one-touch
+                  control of an immersive audio video entertainment system
+                </p>
+              </div>
+              <div className="grid-item-2">
+                <h6>COMMON SOLUTIONS</h6>
+                <p>
+                  Smart Home Control · Media Control · Lighting Control ·
+                  High-Performance Audio · Hidden Audio Video · Universal
+                  Remotes · LED Lighting
+                </p>
+              </div>
+              <div className="grid-item-3">
+                <h6>SAMPLE SOLUTIONS</h6>
+                <div className="icon-grid">
+                  <div>
+                    <i className="fa-regular fa-lightbulb"></i>
+                  </div>
+                  <div>
+                    <i className="fa-brands fa-codepen"></i>
+                  </div>
+                  <div>
+                    <i className="fa-solid fa-house"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src="assets/images/indoor-7.webp" alt="" />
+      </div>
     </div>
   );
 };
