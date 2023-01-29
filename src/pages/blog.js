1ded3084bf32c6b3e5c90ada0aae43056a3f71b3
sakhawat9 <sakhawathossain7969@gmail.com>
2023-01-29 16:31:20 +0600

    update

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
-const Article = React.lazy(() => import("../components/article"));
 const LoadingSpinner = React.lazy(() =>
   import("../components/loading-spinner")
 );
@@ -27,7 +25,28 @@ const Blog = () => {
         {data.length === 0 ? (
           <LoadingSpinner />
         ) : (
-          data.map((blog) => <Article key={blog.id} blog={blog} />)
+          <>
+          <h1 className="title">Blog </h1>
+          <div className="blog-wrapper">
+            <div className="blog-content">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Perspiciatis eius assumenda at animi explicabo amet! Veniam
+              voluptatem neque dolorum at molestiae repellat, quidem recusandae
+              exercitationem harum fuga accusamus soluta? Culpa in laborum,
+              eligendi quis maiores numquam ea optio corrupti ipsam explicabo
+              incidunt porro animi ut nam ipsum, voluptas rem amet. Ipsa,
+              perferendis. Voluptatum corporis, delectus rerum a quisquam vero
+              quam recusandae voluptates labore, aliquam magni corrupti
+              repellendus, quidem tempora rem?
+            </div>
+            <div className="blog-sidebar">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium animi ut nesciunt sint alias eveniet molestias eos!
+              Obcaecati aperiam, animi non nihil consequuntur doloribus
+              accusamus ducimus illum commodi sunt assumenda.
+            </div>
+          </div>
+          </>
         )}
       </main>
     </div>
