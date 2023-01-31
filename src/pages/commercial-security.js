295465b675fe615de81da296b7c5c790914860ed
sakhawat9 <sakhawathossain7969@gmail.com>
2023-01-31 15:35:18 +0600

    add control 4

--- src/pages/commercial-security.js
+++ src/pages/commercial-security.js
@@ -35,37 +35,31 @@ const CommercialSecurity = () => {
                           KEEP YOU IN THE KNOW 24/7.
                         </p>
                       </div>
-
                       <p>
                         <a aria-label="Scroll down01" href="#1">
                           Alarm Systems
                         </a>
                       </p>
-
                       <p>
                         <a aria-label="Scroll down02" href="#2">
                           Surveillance
                         </a>
                       </p>
-
                       <p>
                         <a aria-label="Scroll down03" href="#3">
                           Entry Systems / Access Control{" "}
                         </a>
                       </p>
-
                       <p>
                         <a aria-label="Scroll down04" href="#4">
                           Remote Access / Monitoring
                         </a>
                       </p>
-
                       <p>
                         <a aria-label="Scroll down05" href="#5">
                           Outdoor Perimeter Protection
                         </a>
                       </p>
-
                       <p>
                         <a aria-label="Scroll down06" href="#6">
                           Motion Detection
@@ -109,23 +103,21 @@ const CommercialSecurity = () => {
                   <h2>Business Alarm Systems</h2>
 
                   <p className="pointsmade homeauto">
-                  SECURE YOUR FACILITIES WITH THE BEST COMMERCIAL GRADE SECURITY SOLUTIONS
-
+                    SECURE YOUR FACILITIES WITH THE BEST COMMERCIAL GRADE
+                    SECURITY SOLUTIONS
                   </p>
                   <ul className="commercial-automation-ul">
                     <li>
-                    Reduce the threat from career burglars with a professionally installed and monitored system
-
+                      Reduce the threat from career burglars with a
+                      professionally installed and monitored system
                     </li>
                     <li>
-                    Use one-tap control from a touch screen or mobile device to arm and disarm your business alarm system from anywhere
-
-
+                      Use one-tap control from a touch screen or mobile device
+                      to arm and disarm your business alarm system from anywhere
                     </li>
                     <li>
-                    Receive security notifications for any suspicious activity on your property in Seattle, WA
-
-
+                      Receive security notifications for any suspicious activity
+                      on your property in Seattle, WA
                     </li>
                   </ul>
                 </div>
@@ -165,7 +157,8 @@ const CommercialSecurity = () => {
                   <h2>Surveillance</h2>
 
                   <p className="pointsmade homeauto">
-                  ACCESS REAL-TIME AND RECORDED FOOTAGE ANYTIME FOR SECURITY AND QUALITY CONTROL
+                    ACCESS REAL-TIME AND RECORDED FOOTAGE ANYTIME FOR SECURITY
+                    AND QUALITY CONTROL
                   </p>
 
                   <ul className="commercial-automation-ul">
@@ -174,12 +167,12 @@ const CommercialSecurity = () => {
                       display regardless of distance
                     </li>
                     <li>
-                    Extensive video analytics isolate activity trends and potential threats.
-
+                      Extensive video analytics isolate activity trends and
+                      potential threats.
                     </li>
                     <li>
-                    Keep sensitive surveillance footage secure through encryption protocols.
-
+                      Keep sensitive surveillance footage secure through
+                      encryption protocols.
                     </li>
                   </ul>
                 </div>
@@ -219,20 +212,23 @@ const CommercialSecurity = () => {
                   <h2>Commercial Entry Systems / Access Control</h2>
 
                   <p className="pointsmade homeauto">
-                  CONTROL AND MONITOR ACCESS TO YOUR PROPERTY WITH INNOVATIVE TECHNOLOGY
+                    CONTROL AND MONITOR ACCESS TO YOUR PROPERTY WITH INNOVATIVE
+                    TECHNOLOGY
                   </p>
                   <ul className="commercial-automation-ul">
                     <li>
-                    Ensure authorized access only via keycards, codes, biometrics or mobile apps
-
+                      Ensure authorized access only via keycards, codes,
+                      biometrics or mobile apps
                     </li>
                     <li>
-                    Detailed entry logs and security alerts keep you informed of who comes and goes throughout your property in Seattle, WA
-
+                      Detailed entry logs and security alerts keep you informed
+                      of who comes and goes throughout your property in Seattle,
+                      WA
                     </li>
                     <li>
-                    Detailed entry logs and security alerts keep you informed of who comes and goes throughout your property in Seattle, WA
-
+                      Detailed entry logs and security alerts keep you informed
+                      of who comes and goes throughout your property in Seattle,
+                      WA
                     </li>
                   </ul>
                 </div>
@@ -272,22 +268,22 @@ const CommercialSecurity = () => {
                   <h2>Remote Access / Monitoring</h2>
 
                   <p className="pointsmade homeauto">
-                  TAKE YOUR BUSINESS SECURITY WITH YOU WHEREVER YOU GO WITH DEDICATED APPS
-
+                    TAKE YOUR BUSINESS SECURITY WITH YOU WHEREVER YOU GO WITH
+                    DEDICATED APPS
                   </p>
 
                   <ul className="commercial-automation-ul">
                     <li>
-                    Alarm systems are monitored around the clock to assist in case of an emergency
-
+                      Alarm systems are monitored around the clock to assist in
+                      case of an emergency
                     </li>
                     <li>
-                      Notifications via text or email alert you of any emergencies or suspicious activity
-
+                      Notifications via text or email alert you of any
+                      emergencies or suspicious activity
                     </li>
                     <li>
-                    Have access to surveillance footage and security event data from anywhere
-
+                      Have access to surveillance footage and security event
+                      data from anywhere
                     </li>
                   </ul>
                 </div>
@@ -327,21 +323,21 @@ const CommercialSecurity = () => {
                   <h2>Outdoor Perimeter Protection</h2>
 
                   <p className="pointsmade homeauto">
-                  IMPLEMENT PERIMETER PROTECTIONS TO KEEP THREATS FROM REACHING YOUR PROPERTY
-
+                    IMPLEMENT PERIMETER PROTECTIONS TO KEEP THREATS FROM
+                    REACHING YOUR PROPERTY
                   </p>
                   <ul className="commercial-automation-ul">
                     <li>
-                    Fence, wall and perimeter sensors quickly inform you of suspicious activity
-
+                      Fence, wall and perimeter sensors quickly inform you of
+                      suspicious activity
                     </li>
                     <li>
-                    Enforce strict access control protocols with state-of-the-art gate entry systems
-
+                      Enforce strict access control protocols with
+                      state-of-the-art gate entry systems
                     </li>
                     <li>
-                    Video surveillance systems capture threats the moment they enter protected areas
-
+                      Video surveillance systems capture threats the moment they
+                      enter protected areas
                     </li>
                   </ul>
                 </div>
@@ -380,21 +376,22 @@ const CommercialSecurity = () => {
                   <h2>Motion Detection</h2>
 
                   <p className="pointsmade homeauto">
-                  GUARD YOUR PROPERTY AGAINST INTRUSION AT ALL TIMES WITH STRATEGIC MOTION SENSORS
+                    GUARD YOUR PROPERTY AGAINST INTRUSION AT ALL TIMES WITH
+                    STRATEGIC MOTION SENSORS
                   </p>
 
                   <ul className="commercial-automation-ul">
                     <li>
-                    Ensure a proactive level of defense with sensors that monitor movement 24/7
-
+                      Ensure a proactive level of defense with sensors that
+                      monitor movement 24/7
                     </li>
                     <li>
-                    Lock external doors, turn lights on and activate the alarm when motion is detected
-
+                      Lock external doors, turn lights on and activate the alarm
+                      when motion is detected
                     </li>
                     <li>
-                    Instantly receive an alert when motion detectors go off in sensitive areas
-
+                      Instantly receive an alert when motion detectors go off in
+                      sensitive areas
                     </li>
                   </ul>
                 </div>
@@ -407,12 +404,12 @@ const CommercialSecurity = () => {
               <i className="fa-solid fa-arrow-up"></i>
             </a>
             <h2>
-            Protect Your Facilities With Commercial-Grade Security Solutions
-
+              Protect Your Facilities With Commercial-Grade Security Solutions
             </h2>
             <p className="minigray">
-            Are you looking for a more proactive way to protect your business? Let us design a security solution tailored to your unique needs featuring the latest security technology.
-
+              Are you looking for a more proactive way to protect your business?
+              Let us design a security solution tailored to your unique needs
+              featuring the latest security technology.
             </p>
             <a className="button" href="/contact-us">
               Request an Expert Consult Today
---
+++ src/pages/control4.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+const Control4 = () => {
+  return (
+    <div id="site-content">
+      <div className="post-inner thin commercial-page">
+        <div className="entry-content">
+          <div className="is-layout-flow wp-block-group homevideo servicespages commercial-automantion-video">
+            <div className="wp-block-group__inner-container commercial-automantion-video">
+              <video
+                id="video-background"
+                className="high4kres"
+                autoPlay
+                loop="loop"
+                muted
+                playsInline=""
+                width="300"
+                height="150"
+              >
+                <source src="assets/video/contol-4.mp4" type="video/mp4" />
+              </video>
+
+              <div className="is-layout-flow wp-block-group homeintrotop centerintro">
+                <div className="wp-block-group__inner-container">
+                  <div
+                    className="is-layout-flow wp-block-group servicessubs eds-on-scroll eds-scroll-visible"
+                    data-aos="fade-up"
+                    data-aos-duration="800"
+                    data-aos-delay="400"
+                  >
+                    <div className="wp-block-group__inner-container">
+                      <h1>EFFORTLESS CONTROL OF WHAT MATTERS MOST</h1>
+                      <div>
+                        <p className="minigray">
+                          Add comfort and security to your luxury Orange County
+                          property with Control4's innovative smart home
+                          solutions. Revamp your lifestyle with cutting-edge
+                          products and features that allow seamless control of
+                          your technology to create the perfect environment for
+                          your family.
+                        </p>
+                      </div>
+                      <p>
+                        <a aria-label="Scroll down01" href="#1">
+                          See our How-To Videos
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="home-standard"
+        style={{
+          backgroundImage: "url(/assets/images/header-brands-control4.webp)",
+        }}
+      >
+        <div className="home-standard-area">
+          <div className="content">
+            <div className="top">
+              <h3> The New Smart Home Standard </h3>
+              <p>
+                Experience effortless, personalized control of your home with
+                fluid, intuitive interfaces and more than a thousand new
+                features that make it easier than ever to craft a smart home
+                experience entirely unique to you.
+              </p>
+            </div>
+            <div className="image">
+              <img src="assets/images/header-brands-control4.webp" alt="" />
+            </div>
+            <div className="bottom">
+              <h4> Have An Existing System? </h4>
+              <h6> Enjoy The Reimagined Smart Home Experience </h6>
+              <a href="/contact-us">Upgrade to OS 3</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Control4;
