92d3db3cb78dac68a039511c1c1884744dc37538
sakhawat9 <sakhawathossain7969@gmail.com>
2023-02-03 15:21:22 +0600

    update blog details

--- src/components/blog-details-categories.js
+++ src/components/blog-details-categories.js
@@ -1,59 +1,32 @@
 import React from "react";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const api_domain = process.env.REACT_APP_DOMAIN;
 
-const BlogDetailsCategories = ({ singleId }) => {
-  const [categories, setCategories] = useState([]);
+const BlogDetailsCategories = ({ singleId, blogDetails }) => {
   const [tags, setTags] = useState([]);
 
-    setTimeout(() => {
-      fetch(`${api_domain}/wp-json/wp/v2/categories?post=${singleId}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setCategories(data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      fetch(`${api_domain}/wp-json/wp/v2/tags?post=${singleId}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setTags(data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }, 1000);
-
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const categoriesResponse = await fetch(
-  //       `${api_domain}/wp-json/wp/v2/categories?post=${singleId}`
-  //     );
-  //     const categoriesData = await categoriesResponse.json();
-  //     setCategories(categoriesData);
-  //     const tagsResponse = await fetch(
-  //       `${api_domain}/wp-json/wp/v2/tags?post=${singleId}`
-  //     );
-  //     const tagsData = await tagsResponse.json();
-  //     setTags(tagsData);
-  //   };
-  //   fetchData();
-  // }, [singleId]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const tagsResponse = await fetch(
+        `${api_domain}/wp-json/wp/v2/tags?post=${singleId}`
+      );
+      const tagsData = await tagsResponse.json();
+      setTags(tagsData);
+    };
+    fetchData();
+  }, [singleId]);
 
   return (
     <>
-      {categories?.length === 0
-        ? ""
-        : categories?.slice(0, 1).map((category) => (
-            <Link key={category?.id} to={`/category/${category?.id}`}>
-              {category.name}
-            </Link>
-          ))}
+      {blogDetails[0]?.post_terms?.slice(0, 1).map((category) => (
+        <Link key={category?.id} to={`/category/${category?.id}`}>
+          {category.name}
+        </Link>
+      ))}
+
       {tags?.length === 0
         ? ""
         : tags?.map((tags) => (
