4d01a652f863bfa22d34d7bd3b0faf05425ce921
monirul-fahad <immonirulfahad@gmail.com>
2023-02-07 12:22:30 +0600

    push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AOS from "aos";
 import { useEffect } from "react";
 import AppRouter from "./AppRouter";
 import "../node_modules/aos/dist/aos.css";
+import 'react-modern-drawer/dist/index.css'
 
 function App() {
   useEffect(() => {
