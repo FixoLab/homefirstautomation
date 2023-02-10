cb597e60a4e1e71a2938d9fe74eac7068bd49872
Foysal Imran <fimran2011@gmail.com>
2023-02-10 10:17:09 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import AOS from "aos";
 import { useEffect } from "react";
-import AppRouter from "./AppRouter";
 import "../node_modules/aos/dist/aos.css";
+import AppRouter from "./AppRouter";
 
 function App() {
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
-  return <AppRouter />;
+  return (<AppRouter />);
 }
 
 export default App;
