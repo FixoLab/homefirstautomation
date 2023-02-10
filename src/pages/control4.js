110f7a3f538f82a80c42b865a5a39603a0c7eb06
Foysal Imran <fimran2011@gmail.com>
2023-02-10 09:58:54 +0600

    update

--- src/pages/control4.js
+++ src/pages/control4.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { Autoplay, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/autoplay";
